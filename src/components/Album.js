6ffd30e04d1f5ce9fa853b187db11d962fffd144
Stephen Coyne <scoyne@bw.edu>
2018-02-05 22:46:12 -0500

    assignment-blocjams-next-button-upload for eval

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,15 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max((this.state.album.songs.length - 1), currentIndex + 1);
+    console.log('current index ' + currentIndex + ' , newIndex ' + newIndex );
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
 
   render() {
     return (
@@ -95,6 +104,7 @@ class Album extends Component {
             currentSong={this.state.currentSong}
             handleSongClick={() => this.handleSongClick(this.state.currentSong)}
             handlePrevClick={() => this.handlePrevClick()}
+            handleNextClick={() => this.handleNextClick()}
          />
          
       </section>
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -5,13 +5,13 @@ class PlayerBar extends Component {
     return (
         <section className="player-bar">
             <section id="buttons">
-                <button id="previous" onClick={this.props.handlePrevClick}>
+                <button id="previous" onClick={this.props.handlePrevClick} >
                     <span className="ion-skip-backward"></span>
                 </button>
                 <button id="play-pause" onClick={this.props.handleSongClick} >
                     <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
                 </button>
-                <button id="next">
+                <button id="next" onClick={this.props.handleNextClick} >
                     <span className="ion-skip-forward"></span>
                 </button>
             </section>
